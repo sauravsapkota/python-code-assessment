f76a8a5ff61326317e3919bcda6e2f45aea9b528
Saurav Sapkota <saurav.sapkota@togglecorp.com>
2024-11-24 09:48:02 +0545

    Add a solution for question no 1 (two-sum problem)

---
+++ q1_two_sum.py
@@ -0,0 +1,14 @@
+def two_sum(nums, target):
+    complement_map = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in complement_map:
+            return [complement_map[complement], i]
+        complement_map[num] = i
+
+# Example usage
+nums = [2, 7, 11, 15]
+target = 9
+result = two_sum(nums, target)
+print("Indices:", result)
